7b80ea71904810ccddf19f665da11dff64651643
Michael Israel <misrael@dspace.com>
2025-11-03 09:41:05 +0100

    Creates a separate method to add the components to the internal set() (allows for easier customization by subclasses).

--- Python/rtmaps.py
+++ Python/rtmaps.py
@@ -479,13 +479,16 @@ class RTMapsAbstraction(RTMapsWrapper):
         elif sys.platform == "win32":
             super(RTMapsAbstraction, self).__init__("--console")
 
+    def _add_component_internal(self, component_type: str, component_id: str):
+        self._components.add(component_id)
+
     def add_component(self, component_type: str, component_id: str, xpos = None, ypos = None, zpos = 0):
         if self._enable_checks:
             if component_id in self._components:
                 raise RTMapsException("A component with name {} does already exist".format(component_id))
         command = "{} {}".format(component_type, component_id)
         self.parse(command)
-        self._components.add(component_id)
+        self._add_component_internal(component_type, component_id)
         if (xpos is not None) and (ypos is not None):
             command = "set_location {} {:d} {:d} {:d}".format(component_id, int(xpos), int(ypos), int(zpos))
             self.parse(command)
@@ -707,7 +710,7 @@ class RTMapsAbstraction(RTMapsWrapper):
                 line_splitted = line.split()
                 # Check if line contains a component being added
                 if not any(char in line for char in invalid_name_characters) and len(line_splitted) == 2:
-                    self._components.add(line_splitted[1])
+                    self._add_component_internal(line_splitted[0], line_splitted[1])
 
     def _load_rtd(self, file_path):
         self.reset()
@@ -716,7 +719,7 @@ class RTMapsAbstraction(RTMapsWrapper):
         command = "loaddiagram <<{}>>".format(file_path)
         self.parse(command)
         for component_nodes in root.findall('int:Component', namespaces=namespaces):
-            self._components.add(component_nodes.attrib["InstanceName"])
+            self._add_component_internal(component_nodes.attrib["Model"], component_nodes.attrib["InstanceName"])
     
     def reset(self):
         self._components.clear()
